49cb957772c5d579ab4e60c50d718339d5349415
Pavathariniv99 <70529069+Pavathariniv99@users.noreply.github.com>
2023-06-22 10:44:33 +0530

    debug and eventEmitter

--- src/app/Shared/Debug.ts
+++ src/app/Shared/Debug.ts
@@ -1,6 +1,8 @@
 import * as dat from 'lil-gui'
 export default class Debug 
 {
+    active: boolean  
+    ui: dat.GUI | undefined
     constructor()
     {
         this.active = window.location.hash === '#debug'
